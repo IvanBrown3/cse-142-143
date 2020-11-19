dbc7d248613463006c8438480e6490cd2d6f06b1
Ivan Brown <IvanWBrown@hotmail.com>
2020-11-18 16:38:04 -0800

    Working YazInterpreter w/ comments

--- YazInterpreter/YazInterpreter.java
+++ YazInterpreter/YazInterpreter.java
@@ -1,36 +1,37 @@
 //This program will start the Yaz Lanugage interpreter.
 //There are three yaz lang commands that are used to read input from
 //a file and convert them to a new result in a new file. This conversion
-//will be based on the commands function. 
+//will be based on each commands function. 
+//This program will also allow you to view what outputs to the file in the console.
 
 import java.util.Scanner;
 import java.io.*;
 public class YazInterpreter {
-   //Main method prints intoduction
-   //Begins program and directs the user to either enter I V or Q.
-   //Each with its respective fuctions.
+   //Main method prints intoduction of the code and begins the program
+   //Begins program and directs the user to either enter I V or Q
+   //User will enter I to interpret the input file
+   //user will enter V to view the output text in the console
+   //user will enter Q to quit the program
+   //adds throws FileNotFoundException
+   //method creates an scanner to read the input
     public static void main(String[] args) throws FileNotFoundException{
-
+        Scanner inputScanner = new Scanner(System.in);
+        boolean quitProgram = false;
         System.out.println("Welcome to YazInterpreter!");
         System.out.println("You may interpret a YazLang program and output");
         System.out.println("the results to a file or view a previously");
         System.out.println("interpreted YazLang program.");
         System.out.println();
-
-        Scanner inputScanner = new Scanner(System.in);
-        boolean quitProgram = false;
-        
         //Loop until quit.
-        while (!quitProgram)
-        {
+        while (!quitProgram){
             System.out.print("(I)nterpret YazLang program, (V)iew output, (Q)uit? ");
             //Read in option
             String option = inputScanner.nextLine().toLowerCase();
             //two main functional calls, if q then program quits
             if (option.equals("i")) 
-                InterpretFile(inputScanner);
+                interpretFile(inputScanner);
             else if (option.equals("v"))
-                ViewResults(inputScanner);
+                viewResults(inputScanner);
             else if (option.equals("q"))
                 quitProgram = true;
         }
@@ -41,14 +42,10 @@ public class YazInterpreter {
     //adds throws file not found expection
     //method firts sets input and output files using input and output methods
     //while the scanner has a new line to process
-    //if while true, then the line is set too the next line in the file
-    //the command is found by going from the start of the line to where the first space is
-    //sets to lowercase 
-    //string args is set to the positon in the line where it finds the space and adds one to indicate that as an argument
     //if statements to indicate where to sent the line based on the argument.
     //else is if the command is not one of the special yazlang commands
     //closes the print stream which is done by flushing stream and closing
-    public static void InterpretFile(Scanner inputScanner) throws FileNotFoundException {
+    public static void interpretFile(Scanner inputScanner) throws FileNotFoundException {
         // Get input file
         Scanner fileScanner =  getInputFileScanner(inputScanner);
         // Set output file
@@ -56,82 +53,91 @@ public class YazInterpreter {
         // Read each line from Input
         while (fileScanner.hasNextLine()){
             String line = fileScanner.nextLine();
-            String command = line.substring(0,line.indexOf(" ")).toLowerCase();  //CONVERT or RANGE start stop step REPEAT ARG Count
-            String args = line.substring(line.indexOf(" ") + 1);
+            Scanner parseThrough = new Scanner(line);
+            String command = parseThrough.next().toLowerCase();
             
             if (command.equals("convert")){
-                log.println(convert(args));
+                log.println(convert(parseThrough));
             }
             else if (command.equals("range")){
-                log.println(range(args));
+                log.println(range(parseThrough));
             }
             else if (command.equals("repeat")){
-                log.println(repeat(args));
+                log.println(repeat(parseThrough));
             }
             else{
                 //invalid command
-
             }
-
         }
         log.close();
         System.out.println("YazLang interpreted and output to a file!");
         System.out.println();
 
     }
-
-    public static String convert(String args){
-        //If input was 212 F
-        String returnString = "100 C";
-        return returnString;
-        
-    }
-
-    public static String range (String args){
-        String returnString = "100 101 102";
-        return returnString;
-    }
-
-    public static String repeat(String args){
+    
+    //static string method, takes Scanner as a parameter
+    //method is told to conver the arguments when the main method reads the command
+    //returns a new string of the the conversions
+    public static String convert(Scanner fileScanner){
         String returnString = "";
-        //"Hello" 2 "World" 1
-        String cnt;ww
-        // chop off the term and cnt from the args then print the term (cnt) times
-        while (args.length()>0) {
-            //extract the term from the args
-            String term = args.substring(0,args.indexOf(" "));
-            
-            //remove the Term from the args
-            args = args.substring(args.indexOf(" ") + 1);
-            if (args.indexOf(" ") > 0){
-                //extract the cnt from the args
-                cnt = args.substring(0, args.indexOf(" "));
-                
-                //Remove the cnt from the args
-                args = args.substring(args.indexOf(" ")+1);
-            }
-            else {
-                //No more terms after this since there are no more spaces
-                cnt = args;
-                args = "";
-            }
-
-            for (int i = 0 ;i<Integer.parseInt(cnt); i++){
-                returnString += term.replace("\"", "").replace("_"," ");
-            }
+        double tempNum = fileScanner.nextInt();
+        String units = fileScanner.next();
+        if(units.equalsIgnoreCase("f")){
+            tempNum = (tempNum - 32) / 1.8;
+            returnString = (int)(tempNum) + "C";
         }
+        else if(units.equalsIgnoreCase("c")){
+            tempNum = (tempNum * 1.8) + 32;
+            returnString = (int)(tempNum) + "F";
+        }
+        return returnString;
+    }
+    
+    //static string method for range command
+    //returns a new string of the range conversions
+    //passes the file Scanner as a parameter
+    //uses the first three arguments of the argument to structure the loop
+    //the arguments follow the pattern of start stop and increment
+    public static String range (Scanner fileScanner){
+        String returnString = "";
+        int initial = fileScanner.nextInt();
+        int stop = fileScanner.nextInt();
+        int incre = fileScanner.nextInt();
+        for (int i = initial; i < stop; i = i + incre){
+            returnString += (i + " ");
+        }
+        return returnString;
+    }
 
+    //static method to use repeat command
+    //passes scanner filsScanner as a parameter
+    //method removes quotes, prints based on the number argument aquired
+    //method replaces any underscore with a blank space
+    //returns a string of the output of this function based on file input
+    public static String repeat(Scanner fileScanner){
+        String returnString = "";
+        while (fileScanner.hasNext()) {
+            String cnt = "";
+            String term = fileScanner.next();
+            if (term.charAt(0) == '\"'){
+                term = term.substring(1, term.length()-1);
+            }
+            int repeatVal = fileScanner.nextInt();
+            for (int i = 0 ;i < repeatVal; i++){
+                cnt += term;
+            }
+            returnString += cnt.replace("_"," ");
+        }
         return returnString;
     }
 
     //View Results, Takes input scanner from main
     //adds throws file not found excpetion
     //while loop that runs the scanner through each line, prints each line from scanner into console
-    public static void ViewResults(Scanner inputScanner) throws FileNotFoundException{
+    public static void viewResults(Scanner inputScanner) throws FileNotFoundException{
         // Get file
         Scanner fileScanner = getInputFileScanner(inputScanner);
         System.out.println();
-        
         while (fileScanner.hasNextLine()){
             System.out.println(fileScanner.nextLine());
         }
@@ -146,7 +152,6 @@ public class YazInterpreter {
     public static PrintStream getLogger(Scanner inputScanner) throws FileNotFoundException{
         System.out.print("Output file name: ");
         String fileName = inputScanner.nextLine();
-
         return new PrintStream(new File(fileName));
     }
 
@@ -161,11 +166,9 @@ public class YazInterpreter {
         boolean isValidFile = false;
         File file = null;
         System.out.print("Input file name: ");
-
         while (!isValidFile){
-            //read in file name
-            file = new File (inputScanner.nextLine());
-
+            String inputFile = inputScanner.nextLine();
+            file = new File (inputFile);
             if (!file.exists()){
                 System.out.print("File not found. Try again: ");
             }
